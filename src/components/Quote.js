18dd39c0c1c83750b0e5f9be667a95ae74d15139
Kelvin Bush <kelybush@gmail.com>
2022-05-31 12:23:41 +0000

    refactor quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-function Quote() {
-  return (
-    <div className="quote">
-      <p>“The best way to predict the future is to create it.”</p>
-      <p>- Abraham Lincoln</p>
-    </div>
-  );
-}
+const Quote = () => (
+  <div className="quote">
+    <p>“The best way to predict the future is to create it.”</p>
+    <p>- Abraham Lincoln</p>
+  </div>
+);
 
 export default Quote;
