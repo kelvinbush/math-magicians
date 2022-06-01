10403ec274e11c2297c334b8dc969e9e07d81e81
Kelvin Bush <kelybush@gmail.com>
2022-06-01 17:48:06 +0300

    test quote and home components with jest snapshots

--- src/components/__tests__/components.test.js
+++ src/components/__tests__/components.test.js
@@ -1,7 +1,15 @@
 import renderer from 'react-test-renderer';
 import Quote from '../Quote';
+import Home from '../Home';
 
-it('renders correctly', () => {
-  const tree = renderer.create(<Quote />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Test Quote and Home component', () => {
+  it('should render Quote component correctly', () => {
+    const tree = renderer.create(<Quote />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render Home Component correctly', () => {
+    const tree = renderer.create(<Home />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
