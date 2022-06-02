875f95dbadd21ddf3faa790385d1a4d0de4de02c
Jake Felice <jasonfelice0@gmail.com>
2022-06-02 01:04:32 -0700

    Added two tests for the calculate function

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,7 +1,7 @@
 import calculate from './calculate';
 
 describe('Logic Tests', () => {
-  it('Should perform the calculations correctly', () => {
+  test('1 + 1 should return total 2', () => {
     let obj = {
       total: null,
       next: null,
@@ -13,5 +13,21 @@ describe('Logic Tests', () => {
     obj = calculate(obj, '1');
     obj = calculate(obj, '=');
     expect(obj.total).toEqual('2');
+  });
+  test('500 - 50 should return 450', () => {
+    let obj = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+    obj = calculate(obj, '5');
+    obj = calculate(obj, '0');
+    obj = calculate(obj, '0');
+    obj = calculate(obj, '-');
+    obj = calculate(obj, '5');
+    obj = calculate(obj, '0');
+    obj = calculate(obj, '=');
+    expect(obj.total).toEqual('450');
   });
 });
