fdae6f59bcc924af14d4eabb42c478186292c6a9
Kelvin Bush <kelybush@gmail.com>
2022-06-02 12:58:15 +0300

    add tests for subtraction and division functionalities

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,12 +1,20 @@
-import operater from './operate';
+import operate from './operate';
 
-describe('Operater Test', () => {
+describe('Operate Test', () => {
   test('1 + 1 should return 2', () => {
-    const total = operater(1, 1, '+');
+    const total = operate(1, 1, '+');
     expect(total).toEqual('2');
   });
   test('50 x 10 should return 500', () => {
-    const total = operater(50, 10, 'x');
+    const total = operate(50, 10, 'x');
     expect(total).toEqual('500');
+  });
+  test('50 - 10 should return 40', () => {
+    const total = operate(50, 10, '-');
+    expect(total).toEqual('40');
+  });
+  test('50 ÷ 10 should return 5', () => {
+    const total = operate(50, 10, '÷');
+    expect(total).toEqual('5');
   });
 });
